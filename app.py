13205adbe9c3b45634ac3564a182d969cb80d7ad
flaskur <flaskur@gmail.com>
2020-10-05 01:34:06 -0400

    added cors and tested connect to front end locally

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
+@cross_origin()
 def index():
 	values = {
 		'message': 'some message',
@@ -14,12 +18,29 @@ def index():
 	return jsonify(values)
 
 @app.route('/check', methods=['POST'])
+@cross_origin()
 def postCheck():
 	data = request.json # originally passed in as a json string, but converted to json obj
 
 	print(data) # should be a json object?
 
 	return jsonify(data)
+
+@app.route('/search', methods=['POST'])
+@cross_origin()
+def postSearch():
+	data = request.json # from request body
+
+	print(data)
+
+	# invoke all scrape util funcs concurrently?
+
+	response = {
+		'random': [1,2,3]
+	}
+
+	return jsonify(response)
+
 
 if __name__ == '__main__':
 	app.run(host='localhost', port=5000)
