23343fbbe18c08975f8bc080d645589b27589a01
flaskur <flaskur@gmail.com>
2020-10-27 16:09:50 -0400

    added error handling for residence record, changed to headless mode

--- util/scrape_salt.py
+++ util/scrape_salt.py
@@ -1,17 +1,19 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import ActionChains
-import time
-
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 def scrapeSalt(house_num, street_name):
 	address = f'{house_num} {street_name}'.lower()
 
-	# convert this to a headless browser
-	browser = webdriver.Chrome(ChromeDriverManager().install())
+	options = Options()
+	options.headless = True
+	browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options) # headless mode
+	# browser = webdriver.Chrome(ChromeDriverManager().install()) # opens browser
 
 	# navigate to start webpage --> avoids iframe
 	url = 'https://slco.org/assessor/new/searchiframe.cfm'
@@ -113,28 +115,48 @@ def scrapeSalt(house_num, street_name):
 	print(last_history, second_last_history)
 	print()
 
+	central_ac = None
+	heating = None
+	owner_occupied = None
+	total_rooms = None
+	bedrooms = None
+	full_baths = None
+	three_quarters_baths = None
+	half_baths = None
+	num_kitchens = None
+	fire_places = None
+	year_built = None
+	percent_complete = None
+	main_floor_area = None
+	above_ground_area = None
+	basement_area = None
+	finished_basement_area = None
+
 	# scrape residence record
-	residence_record = browser.find_element_by_css_selector('div#residencetable')
+	try:
+		residence_record = browser.find_element_by_css_selector('div#residencetable')
+		central_ac = browser.execute_script('return arguments[0].children[1].children[0].children[0].children[4].children[0].innerText', residence_record)
+		heating = browser.execute_script('return arguments[0].children[1].children[0].children[0].children[5].children[0].innerText', residence_record)
+		owner_occupied = browser.execute_script('return arguments[0].children[1].children[0].children[0].children[6].children[0].innerText', residence_record)
+		total_rooms = browser.execute_script('return arguments[0].children[1].children[0].children[0].children[8].children[0].innerText', residence_record)
+		bedrooms = browser.execute_script('return arguments[0].children[1].children[0].children[0].children[9].children[0].innerText', residence_record)
 
-	central_ac = browser.execute_script('return arguments[0].children[1].children[0].children[0].children[4].children[0].innerText', residence_record)
-	heating = browser.execute_script('return arguments[0].children[1].children[0].children[0].children[5].children[0].innerText', residence_record)
-	owner_occupied = browser.execute_script('return arguments[0].children[1].children[0].children[0].children[6].children[0].innerText', residence_record)
-	total_rooms = browser.execute_script('return arguments[0].children[1].children[0].children[0].children[8].children[0].innerText', residence_record)
-	bedrooms = browser.execute_script('return arguments[0].children[1].children[0].children[0].children[9].children[0].innerText', residence_record)
+		full_baths = browser.execute_script('return arguments[0].children[1].children[0].children[1].children[0].children[0].innerText', residence_record)
+		three_quarters_baths = browser.execute_script('return arguments[0].children[1].children[0].children[1].children[4].children[0].innerText', residence_record)
+		half_baths = browser.execute_script('return arguments[0].children[1].children[0].children[1].children[2].children[0].innerText', residence_record)
+		num_kitchens = browser.execute_script('return arguments[0].children[1].children[0].children[1].children[3].children[0].innerText', residence_record)
+		fire_places = browser.execute_script('return arguments[0].children[1].children[0].children[1].children[4].children[0].innerText', residence_record)
+		year_built = browser.execute_script('return arguments[0].children[1].children[0].children[1].children[5].children[0].innerText', residence_record)
 
-	full_baths = browser.execute_script('return arguments[0].children[1].children[0].children[1].children[0].children[0].innerText', residence_record)
-	three_quarters_baths = browser.execute_script('return arguments[0].children[1].children[0].children[1].children[4].children[0].innerText', residence_record)
-	half_baths = browser.execute_script('return arguments[0].children[1].children[0].children[1].children[2].children[0].innerText', residence_record)
-	num_kitchens = browser.execute_script('return arguments[0].children[1].children[0].children[1].children[3].children[0].innerText', residence_record)
-	fire_places = browser.execute_script('return arguments[0].children[1].children[0].children[1].children[4].children[0].innerText', residence_record)
-	year_built = browser.execute_script('return arguments[0].children[1].children[0].children[1].children[5].children[0].innerText', residence_record)
+		percent_complete = browser.execute_script('return arguments[0].children[1].children[0].children[2].children[9].children[0].innerText', residence_record)
 
-	percent_complete = browser.execute_script('return arguments[0].children[1].children[0].children[2].children[9].children[0].innerText', residence_record)
-
-	main_floor_area = browser.execute_script('return arguments[0].children[1].children[0].children[3].children[0].children[0].innerText', residence_record)
-	above_ground_area = browser.execute_script('return arguments[0].children[1].children[0].children[3].children[3].children[0].innerText', residence_record)
-	basement_area = browser.execute_script('return arguments[0].children[1].children[0].children[3].children[4].children[0].innerText', residence_record)
-	finished_basement_area = browser.execute_script('return arguments[0].children[1].children[0].children[3].children[5].children[0].innerText', residence_record)
+		main_floor_area = browser.execute_script('return arguments[0].children[1].children[0].children[3].children[0].children[0].innerText', residence_record)
+		above_ground_area = browser.execute_script('return arguments[0].children[1].children[0].children[3].children[3].children[0].innerText', residence_record)
+		basement_area = browser.execute_script('return arguments[0].children[1].children[0].children[3].children[4].children[0].innerText', residence_record)
+		finished_basement_area = browser.execute_script('return arguments[0].children[1].children[0].children[3].children[5].children[0].innerText', residence_record)
+	except Exception as err:
+		print('failed to get residence record')
+		print(err)
 
 	print('CENTRAL AC:', central_ac)
 	print('HEATING:', heating)
@@ -164,90 +186,35 @@ def scrapeSalt(house_num, street_name):
 		tax_district=tax_district,
 		land_value=land_value,
 		building_value=building_value,
-		market_value=market_value
+		market_value=market_value,
+		central_ac=central_ac,
+		heating=heating,
+		owner_occupied=owner_occupied,
+		total_rooms=total_rooms,
+		bedrooms=bedrooms,
+		full_baths=full_baths,
+		three_quarters_baths=three_quarters_baths,
+		half_baths=half_baths,
+		num_kitchens=num_kitchens,
+		fire_places=fire_places,
+		year_built=year_built,
+		percent_complete=percent_complete,
+		main_floor_area=main_floor_area,
+		above_ground_area=above_ground_area,
+		basement_area=basement_area,
+		finished_basement_area=finished_basement_area
 	)
 
 	return scrape_info
 
-info = scrapeSalt('2451', 'e ellisonwoods ave')
+# info = scrapeSalt('2451', 'e ellisonwoods ave')
 
-print(info)
+# print(info)
+# print(info['url'])
 
+info2 = scrapeSalt('4068', 'S 3200 W') # I expect residence record to fail, ignores and returns as None
 
-# EXTRACT URL LINK AT THE END RETURN
+print(info2)
+print(info2['url'])
 
-
-# for link in links_white:
-# 	link_address = browser.execute_script('return arguments[0].innerText', link)
-# 	print(link_address)
-# 	if address in link_address.lower():
-# 		print('found match', address, link_address)
-# 		found_count += 1
-# 		ActionChains(browser).click(link).perform()
-
-# for link in links_grey:
-# 	link_address = browser.execute_script('return arguments[0].innerText', link)
-# 	print(link_address)
-# 	if address in link_address.lower():
-# 		print('found match', address, link_address)
-# 		found_count += 1
-# 		ActionChains(browser).click(link).perform()
-
-# # so it checks every one and there might be duplicates, but it technically should link to the last one
-# if found_count < 1:
-# 	print('found nothing')
-# 	# should return error here
-
-
-# # you might want to do browser get url instead
-# detail = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, 'div#detailDiv')))
-# print(detail.innerText)
-
-# wait = WebDriverWait(browser, 10)
-# results_white = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, 'tr.resultsWhite')))
-# results_grey = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, 'tr.resultsGrey')))
-# # time.sleep(4)
-
-
-# # results_white = browser.find_elements_by_css_selector('tr.resultsWhite div div a')
-# # results_grey = browser.find_element_by_css_selector('tr.resultsGrey div div a')
-
-# for result in results_white:
-# 	result_address = browser.execute_script('return arguments[0].children[0].children[2].children[0].children[0].innerText', result)
-# 	print('result address', result_address)
-# 	if address in result_address.lower():
-# 		print('found a match', address, result_address)
-
-
-# for result in results_grey:
-# 	result_address = browser.execute_script('return arguments[0].children[0].children[2].children[0].children[0].innerText', result)
-# 	print('result address', result_address)
-# 	if address in result_address.lower():
-# 		print('found a match', address, result_address)
-
-# click on a link goes to new tab, might be an issue, also means we can shorten the css selector
-
-# account_rows = browser.find_elements_by_css_selector('table tbody tr')
-
-# num_results = browser.find_element_by_css_selector('div#resultsDiv div strong') # breaks here for some reason, query selection isn't working
-# print('num results is', num_results.innerText)
-
-# if num_results == '0':
-# 	print('there are zero results, shoudl return an empty dict with no info or an error')
-
-# # for row, index in account_rows:
-# # 	# skip every even index since top is header and each table row has an associated image container 
-
-# # you could either access row by num based on num_results, or using the resultsWhite and resultsGrey classes
-# for i in range(1, int(num_results) + 1):
-# 	# should be a try except case...
-# 	id = 'resultBlock' + str(i)
-# 	detail_type = 'detailType' + str(i)
-# 	print('id is', id)
-# 	row = browser.find_element_by_css_selector(id)
-
-# 	query_arg = f'return arguments[0].querySelector("{detail_type}.innerText")'
-# 	print('query is', query_arg)
-# 	address_name = browser.execute_script(query_arg)
-
-# 	print('address name', address_name)
+# NEED TO VALIDATE CSS SELECTIONS
