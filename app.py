881dc2e774f3944e7ca94ce5151d45c296c4f860
flaskur <flaskur@gmail.com>
2020-11-23 17:11:15 -0500

    added core weber scrape logic

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ import sys
 sys.path.insert(0, '/util/scrape_salt.py')
 sys.path.insert(0, '/util/scrape_utah.py')
 sys.path.insert(0, '/util/scrape_wasatch.py')
+sys.path.insert(0, '/util/scrape_weber.py')
 sys.path.insert(0, '/util/scrape_redfin.py')
 import util.scrape_salt as scrape_salt
 import util.scrape_utah as scrape_utah
 import util.scrape_wasatch as scrape_wasatch
+import util.scrape_weber as scrape_weber
 import util.scrape_redfin as scrape_redfin
 
 app = Flask(__name__)
@@ -54,10 +56,11 @@ def postSearch():
 @app.route('/scrape', methods=['POST'])
 @cross_origin()
 def postScrape():
-	# saltZipcodes = ['84116', '84104', '84105', '84103', '84108', '84102', '84106', '84111', '84115', '84109', '84101', '84112', '84113', '84119', '84044', '84107', '84114', '84117', '84118', '84120', '84121', '84123', '84124', '84128', '84129', '84133', '84138', '84144', '84180', '84132', '84134', '84136', '84139', '84141', '84143', '84148', '84150', '84184', '84189', '84190', '84199', '84110', '84122', '84125', '84126', '84127', '84130', '84131', '84145', '84147', '84151', '84152', '84157', '84158', '84165', '84170', '84171', '84093']
 	saltZipcodes = ['84006', '84020', '84096', '84044', '84047', '84065', '84101', '84102', '84103', '84104', '84105', '84106', '84107', '84108', '84109', '84111', '84112', '84113', '84115', '84116', '84117', '84118', '84119', '84120', '84121', '84123', '84124', '84128', '84144', '84180', '84070', '84092', '84093', '84094', '84095', '84081', '84084', '84088']
 	utahZipcodes = ['84004', '84003', '84013', '84020', '84005', '84626', '84629', '84633', '84526', '84043', '84042', '84664', '84057', '84058', '84097', '84651', '84062', '84601', '84604', '84606', '84653', '84655', '84045', '84660', '84663']
 	wasatchZipcodes = ['84032', '84036', '84049', '84060', '84604', '84082']
+	weberZipcodes = ['84310', '84315', '84317', '84401', '84403', '84404', '84405', '84414', '84067']
+	tooeleZipcodes = ['84022', '84029', '84034', '84069', '84071', '84074', '84080', '84083']
  	
 	data = request.json
 	print(data, data['houseNum'], data['streetName'], data['zipcode'])
@@ -70,6 +73,8 @@ def postScrape():
 		result['accessor'] = scrape_utah.scrapeUtah(data['houseNum'], data['streetName'])
 	elif data['zipcode'] in wasatchZipcodes:
 		result['accessor'] = scrape_wasatch.scrapeWasatch(data['houseNum'], data['streetName'])
+	elif data['zipcode'] in weberZipcodes:
+		result['accessor'] = scrape_weber.scrapeWeber(data['houseNum'], data['streetName'])
 	else:
 		result['accessor'] = {
 			'error': True
---
+++ util/scrape_weber.py
@@ -0,0 +1,148 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import unicodedata
+
+def scrapeWeber(house_num, street_name):
+	try:
+		address = f'{house_num} {street_name}'.lower()
+
+		options = Options()
+		options.headless = True
+		browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options) # headless mode
+		# browser = webdriver.Chrome(ChromeDriverManager().install()) # opens browser
+
+		url = 'http://www3.co.weber.ut.us/psearch/'
+		browser.get(url)
+
+		wait = WebDriverWait(browser, 5)
+
+		address_field = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input#address')))
+		browser.execute_script('arguments[0].value = arguments[1]', address_field, address)
+
+		search_button = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'form#form3 input[value="Search"]')))
+		browser.execute_script('arguments[0].click()', search_button)
+
+		parcel_id_link = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'table[width="95%"] a')))
+		browser.execute_script('arguments[0].click()', parcel_id_link)
+
+		# MAIN PAGE
+		current_url = browser.current_url
+
+		market_value = ''
+		parcel_id = ''
+		owner = ''
+		property_address = ''
+		mailing_address = ''
+		tax_unit = ''
+		property_type = ''
+		built_as_desc = ''
+		stories = ''
+		above_square_footage = ''
+		basement_square_footage = ''
+		square_footage = ''
+		basement_percent_complete = ''
+		garage_square_footage = ''
+		percent_complete = ''
+		exterior = ''
+		roof_cover = ''
+		year_built = ''
+		acreage = ''
+
+		try:
+			table_body = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'table[width="350"] table[width="100%"] tbody')))
+			market_value = browser.execute_script('return arguments[0].children[1].children[0].innerText', table_body)
+
+			table_body2 = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'table[width="100%"] tr:nth-child(4) td')))
+			parcel_id = browser.execute_script('return arguments[0].innerText', table_body2).split('Parcel Nbr: ')[1]
+		except Exception as err:
+			print(err, 'failed market value or parcel id')
+			
+		try:
+			ownership_link = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'td[width="120"] a')))
+			browser.execute_script('return arguments[0].click()', ownership_link)
+
+			owner_table = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'table[width="62%"] tbody')))
+
+			owner = browser.execute_script('return arguments[0].children[0].children[1].innerText', owner_table)
+			property_address = browser.execute_script('return arguments[0].children[2].children[1].innerText', owner_table)
+			mailing_address = browser.execute_script('return arguments[0].children[4].children[1].innerText', owner_table)
+			tax_unit = browser.execute_script('return arguments[0].children[6].children[1].innerText', owner_table)
+		except Exception as err:
+			print(err, 'failed ownership info')
+
+		try:
+			property_link = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'td[width="120"]:nth-child(4) a')))
+			browser.execute_script('return arguments[0].click()', property_link)
+
+			char_table = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'table[bgcolor="#CCCCCC"] tbody')))
+
+			property_type = browser.execute_script('return arguments[0].children[0].children[1].innerText', char_table)
+			built_as_desc = browser.execute_script('return arguments[0].children[1].children[1].innerText', char_table)
+			stories = browser.execute_script('return arguments[0].children[2].children[1].innerText', char_table)
+			above_square_footage = browser.execute_script('return arguments[0].children[3].children[1].innerText', char_table)
+			basement_square_footage = browser.execute_script('return arguments[0].children[4].children[1].innerText', char_table)
+			square_footage = browser.execute_script('return arguments[0].children[5].children[1].innerText', char_table)
+			basement_percent_complete = browser.execute_script('return arguments[0].children[6].children[1].innerText', char_table)
+			garage_square_footage = browser.execute_script('return arguments[0].children[7].children[1].innerText', char_table)
+			percent_complete = browser.execute_script('return arguments[0].children[8].children[1].innerText', char_table)
+			exterior = browser.execute_script('return arguments[0].children[9].children[1].innerText', char_table)
+			roof_cover = browser.execute_script('return arguments[0].children[10].children[1].innerText', char_table)
+			year_built = browser.execute_script('return arguments[0].children[11].children[1].innerText', char_table)
+			acreage = browser.execute_script('return arguments[0].children[12].children[1].innerText', char_table)
+		except Exception as err:
+			print(err, 'failed property characteristics')
+		
+
+		scrape_info = dict(
+			error=False,
+			county='Weber',
+			url=browser.current_url,
+			market_value=market_value,
+			parcel_id=parcel_id,
+			owner=owner,
+			property_address=property_address,
+			mailing_address=mailing_address,
+			tax_unit=tax_unit,
+			property_type=property_type,
+			built_as_desc=built_as_desc,
+			stories=stories,
+			above_square_footage=above_square_footage,
+			basement_square_footage=basement_square_footage,
+			square_footage=square_footage,
+			basement_percent_complete=basement_percent_complete,
+			garage_square_footage=garage_square_footage,
+			percent_complete=percent_complete,
+			exterior=exterior,
+			roof_cover=roof_cover,
+			year_built=year_built,
+			acreage=acreage
+		)
+
+		return scrape_info
+	except Exception as err:
+		return {
+			'error': True
+		}
+
+
+# info1 = scrapeWeber('1141', 'w excalibur way')
+# print(info1)
+
+# info2 = scrapeWeber('1930', 'brinker ave')
+# print(info2)
+
+# info3 = scrapeWeber('585', 'e 1800 n')
+# print(info3)
+
+
+'''
+1141 w excalibur way 84401
+1930 s brinker ave 84401
+585 e 1800 n 84414
+'''
