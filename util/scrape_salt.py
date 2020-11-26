2dd0c6f80aedc9c8e4bc40ddcf5c25762211d492
flaskur <flaskur@gmail.com>
2020-11-26 16:39:45 -0500

    trying heroku config for chrome

--- util/scrape_salt.py
+++ util/scrape_salt.py
@@ -8,13 +8,25 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import unicodedata
 
+GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+
 def scrapeSalt(house_num, street_name):
 	try:
 		address = f'{house_num} {street_name}'.lower()
 
-		options = Options()
-		options.headless = True
-		browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options) # headless mode
+
+		chrome_options = webdriver.ChromeOptions()
+
+		chrome_options.add_argument('--disable-gpu')
+		chrome_options.add_argument('--no-sandbox')
+		chrome_options.binary_location = GOOGLE_CHROME_PATH
+
+		browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+
+		# options = Options()
+		# options.headless = True
+		# browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options) # headless mode
 		# browser = webdriver.Chrome(ChromeDriverManager().install()) # opens browser
 
 		url = 'https://slco.org/assessor/new/searchiframe.cfm'
